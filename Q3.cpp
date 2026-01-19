cad84e9aa5d3eef104cbac9d17282bba6555e26c
theparthtanwar261095 <parth25csu173@ncuindia.edu>
2026-01-19 10:04:09 +0530

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+    cout << "The sum is:"<<a+b;
+    return 0;
+}
