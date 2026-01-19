29509c2cd4a7a586a56bff84c63435873a61e630
theparthtanwar261095 <parth25csu173@ncuindia.edu>
2026-01-19 10:16:45 +0530

    Add files via upload

---
+++ Q4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a:";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
+    cout << "The difference is "<<a-b;
+}
---
+++ Q6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"Enter a:";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
+    cout << "The division is"<< a/b;
+}
